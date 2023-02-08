206d34718402bb33c42500ae2b7be6645713c1c4
NooraAlrashid3 <nooraalrashid3322@gmail.com>
2023-02-08 10:04:05 +0300

    security group

--- 5-pub-sg-config.tf
+++ 5-pub-sg-config.tf
@@ -6,9 +6,28 @@ module "public_services_sg" {
   description = "Allow access to public services"
   vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 80
+      protocol    = "tcp"
+      description = "port80 for load balancer"
+      cidr_blocks = "0.0.0.0/0"
+    }]
 
-  egress_rules = ["all-all"]
+  # egress_with_cidr_blocks = [
+  #   {
+  #     from_port   = 0
+  #     to_port     = 80
+  #     protocol    = "tcp"
+  #     description = "port80 for load balancer"
+  #     cidr_blocks = "0.0.0.0/0"
+  #   }]
+
+
+  # ingress_cidr_blocks = ["0.0.0.0/0"]
+  # ingress_rules       = ["http-80-tcp"]
+
+  egress_rules = ["all-all"] # any protocol, any port
 
 }
