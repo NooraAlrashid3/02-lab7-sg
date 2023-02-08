887823c7d282648ae20df7fe16e9f5b3c691b87b
NooraAlrashid3 <nooraalrashid3322@gmail.com>
2023-02-08 10:18:08 +0300

    app SG

--- 6-app-sg-config.tf
+++ 6-app-sg-config.tf
@@ -1,13 +1,23 @@
-# module "app_services_sg" {
-#   source  = "terraform-aws-modules/security-group/aws"
-#   version = "4.17.1"
+module "app_services_sg" {
+    source  = "terraform-aws-modules/security-group/aws"
+    version = "4.17.1"
 
-#   name        = "app-services-sg"
-#   description = "Allow access to app services"
-#   vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+    name        = "app-services-sg"
+    description = "Allow access to app services"
+    vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
 
-#   ingress_cidr_blocks = ["0.0.0.0/0"]
-#   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
 
-#   egress_rules = ["all-all"]
-# }
+# ingress_with_cidr_blocks = [
+#     {
+#       from_port   = 0
+#       to_port     = 80
+#       protocol    = "tcp"
+#       description = "port80 for load balancer"
+#       cidr_blocks = "0.0.0.0/0"
+#     }]
+#     ingress_cidr_blocks = ["0.0.0.0/0"]
+#     ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+
+    ingress_rules =  ["all-all"]
+    egress_rules = ["all-all"]
+}
