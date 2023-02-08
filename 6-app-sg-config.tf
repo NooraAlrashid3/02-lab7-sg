00c9fb443e376cebf3662353f4dec17e682b05b3
NooraAlrashid3 <nooraalrashid3322@gmail.com>
2023-02-08 10:26:33 +0300

    fixing ingress rules

--- 6-app-sg-config.tf
+++ 6-app-sg-config.tf
@@ -18,6 +18,13 @@ module "app_services_sg" {
 #     ingress_cidr_blocks = ["0.0.0.0/0"]
 #     ingress_rules       = ["http-80-tcp", "https-443-tcp"]
 
-    ingress_rules =  ["all-all"]
+    ingress_with_cidr_blocks = [
+    {
+        from_port   = 0
+        to_port     = 80
+        protocol    = "tcp"
+        description = "port80 for load balancer"
+        cidr_blocks = "0.0.0.0/0"
+    }]
     egress_rules = ["all-all"]
 }
